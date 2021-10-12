26eae073c2e7f3ab536f33c237310bc8ba8e282a
Edison <edisonstuart@gmail.com>
2021-10-12 14:08:53 -0400

    added some comments

--- FirstProject/StringFormating.py
+++ FirstProject/StringFormating.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+from datetime import date #not part of video practice, messing around with python libraries
+
 name = input('Enter your name: ')
 surname = input('Enter your surname: ')
-when = "today"
+
+when = date.today()
+print(type(when))
 
 message = "Hello %s %s!" % (name, surname)
 message = f'Hello {name} {surname}. what\'s up {when}' # only works in python 3.6 +
