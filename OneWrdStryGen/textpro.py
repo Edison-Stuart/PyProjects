02800a2c4735a592659b9595399245a160440a50
Edison <edisonstuart@gmail.com>
2021-10-12 14:53:30 -0400

    first small program. can be used to make a story one sentence at a time

---
+++ OneWrdStryGen/textpro.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+def sentence(phrase):
+    interrogatives = ("how", "waht", "why")
+    capitalized = phrase.capitalize()
+    if phrase.startswith(interrogatives):
+        return "{}?".format(capitalized)
+    else:
+        return "{}.".format(capitalized)
+
+output = []
+while True:
+    user_input = input('Say something: ')
+    if user_input == "/end":
+        break
+    else:
+        output.append(sentence(user_input))
+
+print(" ".join(output))
