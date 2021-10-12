32843bee824453a9a10fb9c1bd4d852e8d5e0f6c
Edison <edisonstuart@gmail.com>
2021-10-11 23:32:32 -0400

    worked on user input

---
+++ FirstProject/InputPractice.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def weather_condition(temperature):
+    if temperature > 7:
+        return "warm"
+    else:
+        return "cold"
+
+# user_input = input("Enter temperature:")   user input will be converted into a string, so function won't work
+
+# print(user_input.lower())    can make changes to the user input with methods
+
+user_input = float(input('Enter temperature:')) # We convert the string into a float so our function will work
+print(weather_condition(user_input)) 
