39a79279c409287c22d8ac67ac848ded50b57c24
Edison <edisonstuart@gmail.com>
2021-10-12 14:10:01 -0400

    worked on for loops and while loops

---
+++ FirstProject/dict_loop.py
@@ -0,0 +1,10 @@
+student_grades = {'Marry': 9.1, 'Sim': 8.8, 'John': 7.5}
+
+# for grades in student_grades.items():  prints out key and value in tuple
+#     print(grades)
+
+# for grades in student_grades.keys():  prints out just the keys in string form
+#     print(grades)
+
+for grades in student_grades.values():
+    print(grades)
---
+++ FirstProject/for_loop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+monday_temperatures = [9.1, 8.8, 7.6]
+
+
+    # This code would be inefficient and take 
+        # N lines of code for N values in the array
+# print(round(monday_temperatures[0])) 
+# print(round(monday_temperatures[1]))
+# print(round(monday_temperatures[2]))
+
+for temperature in monday_temperatures:
+    print(round(temperature))
+
+for letter in 'hello':
+    print(letter.title())
