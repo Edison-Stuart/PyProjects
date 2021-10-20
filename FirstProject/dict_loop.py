55b39c9ca941c2951869db1422ca7f0e6a69b5b2
Edison <edisonstuart@gmail.com>
2021-10-20 16:09:54 -0400

    fixed function to be easier to test

--- FirstProject/dict_loop.py
+++ FirstProject/dict_loop.py
@@ -11,21 +11,45 @@ student_grades = {'Marry': 9.1, 'Sim': 8.8, 'John': 7.5}
 # for grades in student_grades.values():
 #     print(grades)
 
-def data_printer(data):
+# commented out because not efficent use of functions
+# def data_printer(data):
+#     out = []
+#     answer = input("Do you want keys, values, or items?: ")
+#     if answer in("keys key Keys Key"):
+#         for key in data.keys():
+#             out.append(key)
+#         return out
+#     elif answer in("values value Values Value"):
+#         for value in data.values():
+#             out.append(value)
+#         return out
+#     elif answer in("items item Items Item"):
+#         print("answer item")
+#         for item in data.items():
+#             out.append(item)
+#         return out
+#     else:
+#         return "Not an option, please enter key value or item"
+
+def data_printer_item(data):
     out = []
-    answer = input("Do you want keys, values, or items?: ")
-    if answer in("keys key Keys Key"):
-        for key in data.keys():
-            out.append(key)
-        return out
-    elif answer in("values value Values Value"):
-        for value in data.values():
-            out.append(value)
-        return out
-    elif answer in("items item Items Item"):
-        print("answer item")
-        for item in data.items():
-            out.append(item)
-        return out
-    else:
-        return "Not an option, please enter key value or item"
+    for item in data.items():
+        out.append(item)
+    return out
+
+def data_printer_key(data):
+    out = []
+    for key in data.keys():
+        out.append(key)
+    return out
+
+def data_printer_value(data):
+    out = []
+    for value in data.values():
+        out.append(value)
+    return out
+
+
+
+print(data_printer_value(student_grades))
+
--- FirstProject/tests/test_dict_loop.py
+++ FirstProject/tests/test_dict_loop.py
@@ -1,12 +1,18 @@
-import mock
-from ..dict_loop import data_printer
+from ..dict_loop import data_printer_value
+from ..dict_loop import data_printer_item
+from ..dict_loop import data_printer_key
+
 
 student_grades = {'Marry': 9.1, 'Sim': 8.8, 'John': 7.5}
+student_grades2 = ('Marry', 9.1, 'Sim', 8.8, 'John', 7.5)
 
-def test_data_printer():
-    with mock.patch.object(__builtins__, 'input', lambda: "items"):
-        assert data_printer(student_grades) == [('Marry', 9.1), ('Sim', 8.8), ('John', 7.5)]
-    with mock.patch.object(__builtins__, 'input', lambda: "keys"):
-        assert data_printer(student_grades) == ['Marry', 'Sim', 'John']
-    with mock.patch.object(__builtins__, 'input', lambda: 'values'):
-        assert data_printer(student_grades) == [9.1, 8.8, 7.5]
+def test_data_printer_item():
+    assert data_printer_item(student_grades) == [('Marry', 9.1), ('Sim', 8.8), ('John', 7.5)]
+
+
+def test_data_printer_key():
+    assert data_printer_key(student_grades) == ['Marry', 'Sim', 'John']
+
+def test_data_printer_value():
+    assert data_printer_value(student_grades) == [9.1, 8.8, 7.5]
+
